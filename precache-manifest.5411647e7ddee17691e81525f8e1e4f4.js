75c6dae8c13d6239c653d80b169b0288129bf423
JonathanLeeWH <JonathanLeeWH@users.noreply.github.com>
2021-09-20 15:58:35 +0000

    deploy: 5c75b9580bb42f8bad879c715ff6dc2b3fcbcd8d

---
+++ precache-manifest.5411647e7ddee17691e81525f8e1e4f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68825f2afd3d1be40ba576e400376bba",
+    "url": "https://learnsoftwaredevelopment.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "b8a0afc534870ec7a4df",
+    "url": "https://learnsoftwaredevelopment.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "807746ef1d28a65a97ee",
+    "url": "https://learnsoftwaredevelopment.github.io/task-management-frontend/static/js/2.609a45ea.chunk.js"
+  },
+  {
+    "revision": "b8a0afc534870ec7a4df",
+    "url": "https://learnsoftwaredevelopment.github.io/task-management-frontend/static/js/main.ea819b67.chunk.js"
+  },
+  {
+    "revision": "ef5823340936504237d8",
+    "url": "https://learnsoftwaredevelopment.github.io/task-management-frontend/static/js/runtime~main.06a24a49.js"
+  }
+]);
